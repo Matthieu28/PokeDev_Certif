7b79c8831b8fd8aecf48f8a667f248faeacf1c31
Matthieu <Matthieu.Maginot@gmail.com>
2023-04-23 15:17:05 +0200

    the problems

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const userControllers = require("../controllers/userControllers");
 
 router.get("/", userControllers.browse);
+router.get("/all/:userId", userControllers.readById);
 router.get("/:id", userControllers.read);
 router.put("/:id", userControllers.edit);
 router.post("/", userControllers.add);
